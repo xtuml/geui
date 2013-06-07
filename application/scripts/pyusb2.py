6ec60f2f51953e047d832ac96ca2d0d8a2ac0b96
Jason Rhodes <jrhodes9@gmail.com>
2013-06-07 13:16:41 -0400

    A more fleshed out example after looking at different examples on the web.  This also fixes the problem with needing to reset the hardware before talking to it again (detaching from the kernel). #16

--- application/scripts/pyusb2.py
+++ application/scripts/pyusb2.py
@@ -1,22 +1,67 @@
+#=====================================================================
+# pyusb code
+#
+# Lots of credit to this site for making some sense out of PyUSB...
+# http://www.micahcarrick.com/credit-card-reader-pyusb.html
+#
+# usage (from Linux command line):
+#   sudo python pyusb2.py
+#
+# Note:  Since PyUSB allows direct access to hardware certain calls
+#        require root permissions.  See an explanation here:
+# http://stackoverflow.com/questions/3738173/why-does-pyusb-libusb-require-root-sudo-permissions-on-linux?rq=1
+#=====================================================================
+import sys
 import usb.core
 import usb.util
 
-#use for testing without the client
+VENDOR_ID  = 0x0483             # ST's Vendor ID
+PRODUCT_ID = 0x9999             # pulled out of thin air
 
-#pyusb code
+#=====================================================================
 # find our device
-dev = usb.core.find(idVendor=0x0483, idProduct=0x9999)
+#=====================================================================
+dev = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
 
-# was it found?
 if dev is None:
-    raise ValueError('Device not found')
+    sys.exit("Could not find our device.\n")
 
-dev.detach_kernel_driver(0)
+#=====================================================================
+# If the built-in kernel module has already loaded the HID driver, we
+# need to detach it so we can use it.
+#=====================================================================
+if dev.is_kernel_driver_active(0):
+    try:
+        dev.detach_kernel_driver(0)
+    except usb.core.USBError as e:
+        sys.exit("Could not detatch kernel driver: %s" % str(e))
 
-dev.write(1, 'test', 0)
+#=====================================================================
+# set_configuration() will use the first configuration which is fine
+# because we only have one.  The reset() initializes everything.
+#=====================================================================
+try:
+    dev.set_configuration()
+    dev.reset()
+except usb.core.USBError as e:
+    sys.exit("Could not set configuration: %s" % str(e))
 
-#attempt to read from device
-reply = dev.read(1, 4, 0, 5000)
-print reply
+#=====================================================================
+# Set up some shorthand for accessing the endpoints then print some
+# info to the terminal for debugging.
+#=====================================================================
+EP_IN  = dev[0][(0,0)][0]
+EP_OUT = dev[0][(0,0)][1]
 
+print "EP_IN  addr, max packet size:  0x%02x, %d " % (EP_IN.bEndpointAddress, EP_IN.wMaxPacketSize)
+print "EP_OUT addr, max packet size:  0x%02x, %d " % (EP_OUT.bEndpointAddress, EP_OUT.wMaxPacketSize)
 
+#=====================================================================
+# Do a simple write and read test.
+#=====================================================================
+print "writing..."
+EP_OUT.write('test')
+
+print "reading..."
+data = dev.read(EP_IN.bEndpointAddress, EP_IN.wMaxPacketSize)
+print data
