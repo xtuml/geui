06cddea7566d8fba6942a881917dd1a827ba40dc
Levi Starrett <levi@roxsoftware.com>
2013-08-29 15:45:06 -0400

    Moved CommandLine class to its own file. Edited .gitignore to ignore all log files.

---
+++ application/agent/command.py
@@ -0,0 +1,61 @@
+import eihttp as eihttp
+
+import threading
+import time
+import re
+
+import logging
+
+#thread for executing commands
+class CommandLine(threading.Thread):
+    
+    running = None
+
+    commands = [
+        '"send_wave" ---> Send test wave from test bench to EC.',
+        '"get_version" ---> Send get_version request to EC.',
+        '"add_segment <args>" ---> Add a segment to the current experiment.',
+        '"exit" ---> Quit program.',
+        '"help" ---> Show command help.'
+    ]
+
+    def __init__(self, thread_name='command'):
+        threading.Thread.__init__(self, name=thread_name)
+        self.running = False
+
+    def run(self):
+        self.running = True
+        print 'Type "help" for command list.'
+        while self.running:
+            x = raw_input()
+            if x == 'exit':
+                for t in threading.enumerate():
+                    if t.name == 'agent':
+                        t.q.put([t.exit])
+                        break
+                self.running = False
+            elif x == 'help':
+                for command in self.commands:
+                    print command
+            elif x == 'send_wave':
+                for t in threading.enumerate():
+                    if t.name == 'test':
+                        t.q.put([t.send_wave])
+                        break
+            elif x == 'get_version':
+                for t in threading.enumerate():
+                    if t.name == 'agent':
+                        t.q.put([t.get_version])
+                        break
+            elif re.search('add_segment', x) != None:
+                for t in threading.enumerate():
+                    if t.name == 'agent':
+                        args = x.split(' ')[1:]
+                        t.q.put([eihttp.add_segment, float(args[0]), float(args[1]), float(args[2]), float(args[3]), int(args[4]), 1])
+            else:
+                print 'No command "' + x + '"'
+
+        logger = logging.getLogger('agent_log')
+        logger.info('Exited CommandLine at [' + time.ctime() + ']')
+
+
