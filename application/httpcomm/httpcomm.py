378e35f2f932af7199429fe239b42fc101c71593
Levi Starrett <levi@roxsoftware.com>
2014-05-14 12:55:38 -0400

    Added idle timeout exit feature for background running. Issue #38

--- application/httpcomm/httpcomm.py
+++ application/httpcomm/httpcomm.py
@@ -9,6 +9,8 @@ import agent.thread
 import Queue
 import server
 import json
+import logging
+import time
 
 import eihttp
 
@@ -38,16 +40,34 @@ class HTTPcomm(agent.thread.Thread, eihttp.EIhttp):
     # holds data packets to be sent to GUI
     data_out = None
 
+    # timer variables
+    timer = 0           # if it accumulates to 10 minutes without a command the program will automatically terminate
+    autoexit = False    # whether or not to auto exit on timeout
+    timeout = 0         # number of milliseconds before idle timeout
+
     def __init__(self, name="httpcomm"):
         agent.thread.Thread.__init__(self, name=name)
         self.commands = Queue.Queue()
         self.data_out = Queue.Queue()
         self.server = None
+        self.block = False
+        self.timer = 0
+        self.autoexit = False
+        self.timeout = 0
 
     def initialize(self):
         self.server = server.Server()
-        self.command.server = self.server
+        if self.command != None:
+            self.command.server = self.server
         self.server.start()
+
+    def check(self):
+        if self.timer == self.timeout:
+            logger = logging.getLogger("agent_log")
+            logger.info("Idle timeout [" + time.ctime() + "]")
+            self.exit()
+        if self.autoexit:
+            self.timer += 10
 
     def finalize(self):
         self.server.app.stop()
@@ -57,6 +77,7 @@ class HTTPcomm(agent.thread.Thread, eihttp.EIhttp):
 
     # version response from agent
     def version(self, version):
+        self.timer = 0
         data = {
             "signal": "version",
             "version": version
@@ -65,6 +86,7 @@ class HTTPcomm(agent.thread.Thread, eihttp.EIhttp):
 
     # data packet from agent
     def data(self, points, action):
+        self.timer = 0
         if action == "start" or action == "onepacket":
             data = {
                 "signal": "data",
@@ -82,6 +104,7 @@ class HTTPcomm(agent.thread.Thread, eihttp.EIhttp):
 
     # chart data response from agent
     def update_graph(self, points):
+        self.timer = 0
         data = {
             "signal": "update_graph",
             "points": points
@@ -90,6 +113,7 @@ class HTTPcomm(agent.thread.Thread, eihttp.EIhttp):
 
     # table data response from agent
     def load_table(self, rows, table_id):
+        self.timer = 0
         data = {
             "signal": "load_table",
             "table": table_id,
@@ -99,6 +123,7 @@ class HTTPcomm(agent.thread.Thread, eihttp.EIhttp):
 
     # send GUI list of saved experiment
     def load_experiments(self, experiments):
+        self.timer = 0
         data = {
             "signal": "load_experiments",
             "experiments": experiments
@@ -107,6 +132,7 @@ class HTTPcomm(agent.thread.Thread, eihttp.EIhttp):
 
     # send GUI indication that the upload was a success
     def upload_success(self, name):
+        self.timer = 0
         data = {
             "signal": "upload_success",
             "name": name
@@ -119,73 +145,91 @@ class HTTPcomm(agent.thread.Thread, eihttp.EIhttp):
 
     # download waveform to device
     def download(self):
+        self.timer = 0
         self.agent.q.put([self.agent.download])
 
     # run the experiment
     def run_experiment(self):
+        self.timer = 0
         self.agent.q.put([self.agent.run_experiment])
 
     # get version command sent from GUI 
     def get_version(self):
+        self.timer = 0
         self.agent.q.put([self.agent.get_version])
 
     # save experiment command sent from GUI 
     def save_experiment(self):
+        self.timer = 0
         self.agent.q.put([self.agent.save_experiment])
 
     # get experiments command sent from GUI 
     def get_experiments(self):
+        self.timer = 0
         self.agent.q.put([self.agent.get_experiments])
 
     # request table command sent from GUI 
     def request_table(self, table_id, position):
+        self.timer = 0
         self.agent.q.put([self.agent.request_table, table_id, position])
 
     # open experiment command sent from GUI 
     def open_experiment(self, name):
+        self.timer = 0
         self.agent.q.put([self.agent.open_experiment, name])
 
     # create experiment command sent from GUI 
     def create_experiment(self, name):
+        self.timer = 0
         self.agent.q.put([self.agent.create_experiment, name])
 
     # delete experiment command sent from GUI 
     def delete_experiment(self, name):
+        self.timer = 0
         self.agent.q.put([self.agent.delete_experiment, name])
 
     # allows user to upload their own experiment files
     def upload_file(self, name, contents):
+        self.timer = 0
         self.agent.q.put([self.agent.upload_file, name, contents])
 
     # add pattern command sent from GUI 
     def add_pattern(self, start_value, end_value, rate, duration, repeat_value):
+        self.timer = 0
         self.agent.q.put([self.agent.add_pattern, start_value, end_value, rate, duration, repeat_value])
 
     # delete pattern command sent from GUI 
     def delete_pattern(self, positions):
+        self.timer = 0
         self.agent.q.put([self.agent.delete_pattern, positions])
 
     # move pattern command sent from GUI 
     def move_pattern(self, position, destination):
+        self.timer = 0
         self.agent.q.put([self.agent.move_pattern, position, destination])
 
     # update pattern command sent from GUI 
     def update_pattern(self, repeat_value, position):
+        self.timer = 0
         self.agent.q.put([self.agent.update_pattern, repeat_value, position])
 
     # add segment command sent from GUI 
     def add_segment(self, start_value, end_value, rate, duration, position):
+        self.timer = 0
         self.agent.q.put([self.agent.add_segment, start_value, end_value, rate, duration, position])
 
     # delete segment command sent from GUI 
     def delete_segment(self, positions, pattern):
+        self.timer = 0
         self.agent.q.put([self.agent.delete_segment, positions, pattern])
 
     # move segment command sent from GUI 
     def move_segment(self, position, destination, pattern):
+        self.timer = 0
         self.agent.q.put([self.agent.move_segment, position, destination, pattern])
 
     # update segment command sent from GUI 
     def update_segment(self, start_value, end_value, rate, duration, repeat_value, position):
+        self.timer = 0
         self.agent.q.put([self.agent.update_segment, start_value, end_value, rate, duration, repeat_value, position])
 # --------------------------------------------------------------------------------------------- #
