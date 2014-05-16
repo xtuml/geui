dd12808002d52177309b2f4af20cd1fa431791de
Levi Starrett <levi@roxsoftware.com>
2014-05-16 14:30:49 -0400

    Changed Makefile to compile on linux. made host variable in JS. Issue #40

--- mongoose_server/web/js/demo.js
+++ mongoose_server/web/js/demo.js
@@ -9,13 +9,13 @@ defines incoming side of the eihttp interface
 function start_server() {
     var fileref = document.createElement("script");
     fileref.setAttribute("type","text/javascript");
-    fileref.setAttribute("src", "http://localhost:8888/start_server");
+    fileref.setAttribute("src", "http://" + host + ":8888/start_server");
     document.getElementsByTagName("head")[0].appendChild(fileref);
 }
 
 function launch(data) {
     if (data == "success" || data == "busy") {
         //window.location.replace("http://localhost:8080/");
-        window.location.href = "http://localhost:8080/";
+        window.location.href = "http://" + host + ":8080/";
     }
 }
